f70b4af006474bb13faf08b47fcd866566228dbc
ibnhoshim <158367624+ibnhoshim@users.noreply.github.com>
2025-01-12 00:17:59 +0500

    Create project models

--- news_app/models.py
+++ news_app/models.py
@@ -1,3 +1,31 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return self.name
+
+class News(models.Model):
+    
+    class Status(models.TextChoices):
+        Draft = "DF", "Draft"
+        Published = "PB", "Published"
+        
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to="news/images", blank=True, null=True)
+    category = models.ForeignKey(Categories, on_delete = models.CASCADE)
+    publish_time = models.DateTimeField(default=timezone.now)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.Draft)
+    
+    class Meta:
+        ordering = ["-publish_time"]
+        
+    def __str__(self):
+        return self.title
+    
