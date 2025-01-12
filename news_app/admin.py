0e98db1285581e346866b4470cd15a185053f8a8
ibnhoshim <158367624+ibnhoshim@users.noreply.github.com>
2025-01-12 15:21:42 +0500

    Add models to Django Admin

--- news_app/admin.py
+++ news_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categories, News
 
-# Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'publish_time', 'status']
+    list_filter = ['status', 'publish_time', 'created_time']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish_time'    
+    search_fields = ['title', 'body']
+    ordering = ['status', 'publish_time']
+    
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
